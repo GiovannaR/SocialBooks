2a7ba6c60d49f29f68dbcd5bfbd5ec50ff5c20a0
giovanna <giovannariqueti@gmail.com>
2017-08-02 17:12:15 -0300

    dev-2.02: Capitulo 2.07

---
+++ src/main/java/com/crc/socialbooks/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,34 @@
+package com.crc.socialbooks.com.algaworks.socialbooks.resources;
+
+import com.crc.socialbooks.com.algaworks.socialbooks.domain.Livro;
+import com.crc.socialbooks.com.algaworks.socialbooks.repository.LivrosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/livros")
+public class LivrosResources {
+
+    @Autowired
+    public LivrosRepository livrosRepository;
+
+    @RequestMapping( method = RequestMethod.GET)
+    public List<Livro> listar( ){
+        return livrosRepository.findAll();
+    }
+
+    @RequestMapping( method = RequestMethod.POST)
+    public void salvar (@RequestBody Livro livro){
+        livrosRepository.save(livro);
+    }
+
+    @RequestMapping(value ="/{id}", method = RequestMethod.GET)
+    public Livro buscar(@PathVariable Long id){
+        return livrosRepository.findOne(id);
+    }
+}
